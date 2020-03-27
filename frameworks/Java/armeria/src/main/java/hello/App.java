bb8d9a9bdf80d281623cb6de235560e378251e3d
Bo <openaphid@gmail.com>
2020-03-27 16:52:37 +0800

    disable DB services temporarily

--- frameworks/Java/armeria/src/main/java/hello/App.java
+++ frameworks/Java/armeria/src/main/java/hello/App.java
@@ -6,15 +6,18 @@ import com.linecorp.armeria.server.ServerBuilder;
 import hello.services.HelloService;
 import hello.services.PostgresDbService;
 import hello.services.PostgresFortunesService;
+import io.netty.channel.ChannelOption;
 
 public final class App {
   public static void main(String[] args) {
     ServerBuilder sb = Server.builder();
 
     sb.http(8080)
-      .annotatedService(new HelloService())
-      .annotatedService(new PostgresDbService())
-      .annotatedService(new PostgresFortunesService());
+	    .channelOption(ChannelOption.SO_BACKLOG, 8192)
+	    .channelOption(ChannelOption.SO_REUSEADDR, true)
+      .annotatedService(new HelloService());
+//      .annotatedService(new PostgresDbService())
+//      .annotatedService(new PostgresFortunesService());
 
     Server server = sb.build();
     server.start().join();
